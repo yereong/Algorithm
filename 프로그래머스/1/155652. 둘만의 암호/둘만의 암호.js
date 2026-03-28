2f54728ca88f372f06f7f6fbec9ae7832dfecf64
yereong <lily030624@gmail.com>
2026-03-28 17:29:54 +0900

    [level 1] Title: 둘만의 암호, Time: 0.52 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -0,0 +1,27 @@
+function solution(s, skip, index) {
+    let skipList = skip.split('').map((v)=>v.charCodeAt(0));
+    let list = []
+    for(i=0; i<s.length; i++){
+        let char = s.charCodeAt(i)
+        for(j=0; j<index; j++){
+            char += 1;
+            if(char > 'z'.charCodeAt(0)){
+                    char = 'a'.charCodeAt(0);
+                }   
+            while(skipList.includes(char)){ char+=1;
+                if(char > 'z'.charCodeAt(0)){
+                    char = 'a'.charCodeAt(0);
+                }            
+            }
+            
+        }
+        while(skipList.includes(char)){char+=1;
+            if(char > 'z'.charCodeAt(0)){      
+                char = 'a'.charCodeAt(0);}
+            
+        }
+        
+        list.push(String.fromCharCode(char))
+    }
+    return list.join('')
+}
