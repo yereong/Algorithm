b770bf0bd8c77de031365cb088b49b9a77d06c43
yereong <lily030624@gmail.com>
2026-04-29 01:59:03 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 45.12 ms, Memory: 159 MB -BaekjoonHub

---
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+    const answer = new Array(numbers.length).fill(-1);
+    const stack = []; // 인덱스 저장
+
+    for (let i = 0; i < numbers.length; i++) {
+        while (stack.length && numbers[stack[stack.length - 1]] < numbers[i]) {
+            answer[stack.pop()] = numbers[i];
+        }
+        stack.push(i);
+    }
+
+    return answer;
+}
